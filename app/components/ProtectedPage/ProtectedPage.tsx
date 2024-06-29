1bf32adab1d6d472b372c77be92bd0c2ccd7ab64
Valentin L <ludu.valentin@gmail.com>
2024-06-29 15:38:33 +0300

    Fix types

--- app/components/ProtectedPage/ProtectedPage.tsx
+++ app/components/ProtectedPage/ProtectedPage.tsx
@@ -1,4 +1,4 @@
-import { getServerSession } from "next-auth";
+import { auth } from "@/lib/auth/auth";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage({
@@ -6,10 +6,10 @@ export default async function ProtectedPage({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
+  const session = await auth();
 
   if (!session?.user) {
-    redirect("/api/auth/login");
+    redirect("/login");
   }
 
   return children;
